8dcff25937af182bfa14b970eea76a0393eb32c2
Theodorus-Doni <theodorusdoni4@gmail.com>
2024-09-11 20:51:44 +0700

    sdfsdf

--- docs/README.md
+++ docs/README.md
@@ -1,3 +1,72 @@
-[Report an Issue](https://github.com/your-username/your-repository/issues)
+<br>
+<p align="center">
+  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="https://lncodes.com">
+    <img src="https://github.com/lncodes/docs/blob/master/assets/animations/lncodes-logo-animation.gif" height="175"></img>
+  </a>
+</p>
 
-{{RepositoryName}}
+<h1 align="center">C# Generic</h1>
+<p align="center">
+  <a href="https://github.com/lncodes/csharp-generic/actions/workflows/build-and-publish-dotnet.yml">
+      <img src="https://github.com/lncodes/csharp-generic/actions/workflows/build-and-publish-dotnet.yml/badge.svg">
+  </a>
+  <a href="https://sonarcloud.io/dashboard?id=lncodes_csharp-generic">
+      <img src="https://sonarcloud.io/api/project_badges/measure?project=lncodes_csharp-generic&metric=alert_status">
+  </a>
+</p>
+
+<p align="center">
+  <a href="#introduction">Introduction</a> •
+  <a href="#installation">Installation</a> •
+  <a href="#usage">Usage</a> •
+  <a href="#contribute">Contribute</a> •
+  <a href="#discussion">Discussion</a> •
+  <a href="#license">License</a>
+</p>
+
+<h2 id="introduction">🌟 Introduction</h2>
+
+This repository demonstrates the use of C# Generic through a simple console application. In this repository, a generic class is designed to create multiple quiz variations, each with different data types for the `question` and `answer` variables. By using generic, we can create multiple quiz variations without needing separate classes for each variation, which reduces code duplication, enhances reusability, and ensures type safety.
+
+For more information about C# Generic, check out the blog post: [C# Generic - Last Night Codes](https://www.lncodes.com/csharp-generic).
+
+<h2 id="installation">📦 Installation</h2>
+
+To set up this C# console application, follow these steps:
+
+1. Clone or download the project.
+    ``` bash 
+    git clone https://github.com/lncodes/csharp-generic.git
+    ```
+
+2. Open the project
+    - Open Visual Studio and select File > Open > Project/Solution.
+    
+    - Open the .sln file located in the main folder.
+
+<h2 id="usage">🧑‍💻 Usage</h2>
+
+To run the console application, there's serval options can be use:
+
+1. From Visual Studio
+    - Press Ctrl + F5 to run without debugging.
+
+    - Press F5 or click the green start button to run with debugging.
+    
+2. From .NET CLI
+
+    ```bash
+     dotnet run
+     ```
+
+<h2 id="contribute">🤝 Contribute</h2>
+
+We appreciate your interest in contributing to this project! For detailed guidelines on how to report issues and submit pull requests, please check out our [Contributing Guidelines](CONTRIBUTING.md).
+
+<h2 id="discussion">💬 Discussion</h2>
+
+If you have any questions or want to discuss anything related to this project, visit our [Discussion Forum](https://github.com/lncodes/csharp-generic/discussions). You can also leave comments on [C# Generic - Last Night Codes](https://lncodes.com/csharp-generic/) blog post for more in-depth discussion on this project’s topics.
+
+<h2 id="license"> 📋 License</h2>
+
+This project is licensed under the [MIT License](../LICENSE).<br>
---
+++ src/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+
+namespace Lncodes.Example.Generic;
+
+internal static class Program
+{
+    /// <summary>
+    /// Main entry point for the program.
+    /// </summary>
+    private static void Main()
+    {
+        var quizControllerTypeId = GetRandomQuizControllerId();
+        switch (quizControllerTypeId)
+        {
+            case 0:
+                var quizControllerType1 = new QuizController<string, int, List<int>>("1 + 1 =", 2, [1, 2, 3, 4]);
+                ShowQuizData(quizControllerType1);
+                CheckAnswer(quizControllerType1, int.Parse(Console.ReadLine()));
+                break;
+            case 1:
+                var quizControllerType2 = new QuizController<string, bool, SortedSet<bool>>("Is the sky blue?", true, [true, false]);
+                ShowQuizData(quizControllerType2);
+                CheckAnswer(quizControllerType2, bool.Parse(Console.ReadLine()));
+                break;
+            case 2:
+                var quizControllerType3 = new QuizController<string, string, HashSet<string>>("What is the capital of France?", "Paris", ["London", "Berlin", "Paris", "Madrid"]);
+                ShowQuizData(quizControllerType3);
+                CheckAnswer(quizControllerType3, Console.ReadLine());
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(quizControllerTypeId));
+        }
+    }
+
+    /// <summary>
+    /// Generates a random quiz controller ID.
+    /// </summary>
+    /// <returns cref="int">Random quiz controller ID.</returns>
+    private static int GetRandomQuizControllerId()
+    {
+        const int amountOfEnemyTypes = 3;
+        return RandomNumberGenerator.GetInt32(amountOfEnemyTypes);
+    }
+
+    /// <summary>
+    /// Displays quiz data including question, choices, and answer type.
+    /// </summary>
+    /// <typeparam name="TQuestion">Type of the question.</typeparam>
+    /// <typeparam name="TAnswer">Type of the answer.</typeparam>
+    /// <typeparam name="TChoice">Type of the choices collection.</typeparam>
+    /// <param name="quizController">The quiz controller instance.</param>
+    private static void ShowQuizData<TQuestion, TAnswer, TChoice>(QuizController<TQuestion, TAnswer, TChoice> quizController) where TChoice : ICollection<TAnswer>
+    {
+        var choiceType = quizController.ChoiceCollection.GetType();
+        var choiceName = choiceType.Name.Replace("`1", $"<{choiceType.GenericTypeArguments[0].Name}>");
+        Console.WriteLine($"Question ({quizController.Question.GetType().Name}): {quizController.Question}");
+        Console.Write($"Choice ({choiceName}): ");
+        Console.WriteLine(string.Join(", ", quizController.ChoiceCollection));
+        Console.Write($"Type your answer ({quizController.Answer.GetType().Name}): ");
+    }
+
+    /// <summary>
+    /// Checks if the provided answer is correct and displays the result.
+    /// </summary>
+    /// <typeparam name="TQuestion">Type of the question.</typeparam>
+    /// <typeparam name="TAnswer">Type of the answer.</typeparam>
+    /// <typeparam name="TChoice">Type of the choices collection.</typeparam>
+    /// <param name="quizController">The quiz controller instance.</param>
+    /// <param name="answer">The user-provided answer.</param>
+    private static void CheckAnswer<TQuestion, TAnswer, TChoice>(QuizController<TQuestion, TAnswer, TChoice> quizController, TAnswer answer) where TChoice : ICollection<TAnswer>
+    {
+        Console.WriteLine();
+        quizController.CheckAnswer(answer);
+    }
+}
